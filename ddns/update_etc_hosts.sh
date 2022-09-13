3f14e44a4bad817836b73244cddbf8262a69824a
ehb54 <ehb54@users.noreply.github.com>
2022-09-13 11:45:35 -0500

    Create update_etc_hosts.sh

---
+++ ddns/update_etc_hosts.sh
@@ -0,0 +1,14 @@
+#!/bin/bash                                                                                                                                                                                                           
+
+## config                                                                                                                                                                                                             
+
+HOSTS_FILE="/etc/hosts"
+HOSTS_ENTRY="myhost.local"
+DDNS_NAME="myhost.ydns.eu"
+
+## get current IP                                                                                                                                                                                                     
+IP=`getent hosts $DDNS_NAME | awk '{ print $1 }'`
+
+# echo IP is $IP                                                                                                                                                                                                      
+
+sed -i 's/^.*$HOSTS_ENTRY/$IP $HOSTS_ENTRY/' $HOSTS_FILE
