61429dd748940144ad56dc02b81b61d1372b8ae9
ehb54 <ehb54@users.noreply.github.com>
2022-09-14 14:04:33 -0500

    Update update_ws.sh

--- webbased/update_ws.sh
+++ webbased/update_ws.sh
@@ -15,8 +15,5 @@ IP=$(curl -k --silent https://ydns.io/api/v1/ip)
 OIP=$(curl -k --silent https://$WEB_HOST/$FILENAME)
 
 if [ $IP != $OIP ]; then
-    # echo updating
     ssh $WEB_USER@$WEB_HOST -C "echo $IP > $WEB_ROOT/$FILENAME $EXTRACMDS"
-else
-    # echo no change
 fi
